0ce707ed583305c0936f7842b0b176211196b5a9
Timofey Kuznetsov <tima1702@gmail.com>
2018-06-08 00:12:15 +0300

    Add examples and add some functions

--- index.js
+++ index.js
@@ -47,4 +47,143 @@ const createEmptyMatrix = (rows, columns = 0) => {
   return arr;
 };
 
-export { createAdjacencyList };
+// context - содержимое файла
+// Построить и вывести на экран матрицу смежности.
+// возвращяет матрицу смежности
+const stringToAdjacencyMatrix = (context) => {
+  const count = context.split('\n')[0];
+  const adjacencyMatrix = createEmptyMatrix(count, count);
+
+  this.context.split('\n').forEach((row) => {
+    const [a, b] = row.split(/\s/);
+
+    if (a && b) {
+      adjacencyMatrix[a - 1][b - 1] = 1;
+      adjacencyMatrix[b - 1][a - 1] = 1;
+    }
+  });
+
+  return adjacencyMatrix;
+};
+
+// context - содержимое файла
+// Определить степени всех вершин и вывести список вершин с их степенями.
+// возвращяет Map где key - спискок вершин, value - степень вершины
+const getDegreesOfVertices = (context) => {
+  const result = new Map();
+  createAdjacencyList(context).forEach((value, key) => result.set(key, value.map(i => i.length)));
+  return result;
+};
+
+// context - содержимое файла
+// Определить изолированные вершины и вывести их список.
+// возвращяет массив изолированных вершин
+const getIsolatedVertices = (context) => {
+  const result = [];
+  createAdjacencyList(context).forEach((item, index) => item.length === 0 && result.push(index));
+  return result;
+};
+
+// context - содержимое файла
+// Определить висячие вершины и вывести их список.
+// возвращяет массив висячиx вершин
+const getPendantVertices = (context) => {
+  const result = [];
+  createAdjacencyList(context)
+    .forEach((item, index) => item.length === 1 && item[0] !== index && result.push(index));
+  return result;
+};
+
+// context - содержимое файла
+// Определить висячие ребра и вывести их список.
+// возвращяет массив висячиx ребер
+const getPendantVerges = (context) => {
+  const result = [];
+  createAdjacencyList(context).forEach((item, index) => {
+    if (item.length === 1 && item[0] !== index) result.push([item[0], index]);
+  });
+  return result;
+};
+
+// context - содержимое файла
+// Определить вершины, в которых имеются петли, и вывести список таких вершин с кратностями петель.
+// возвращяет массив объектов
+const getLoops = (context) => {
+  const result = [];
+  createAdjacencyList(context).forEach((item, index) => {
+    const loops = item.filter(i => i === index);
+    if (loops.length > 0) {
+      result.push({ 'вершина': index, 'кратность': loops.length });
+    }
+  });
+  return result;
+};
+
+// context - содержимое файла
+// кратные ребра и выводит их список с кратностями
+// возвращяет массив объектов
+const getMultipleVerges = (context) => {
+  const result = [];
+  createAdjacencyList(context).forEach((item, index) => {
+    const arr = [...new Set(item)];
+    arr.forEach((i) => {
+      const count = item.filter(a => a === i).length;
+      result.push({'ребро': `${i}, ${index}`,'кратность': count});
+    });
+  });
+  return result;
+};
+
+// Приводит исходный граф к форме простого графа:
+// удаляет петли и для кратных ребер оставляет только одно ребро.
+// Для преобразованного графа выводит списки смежности
+// возвращяет список смежности
+const graphToSimpleForm = (context) => {
+  const result = new Map();
+  createAdjacencyList(context).forEach((row, index) => {
+    const array = row.filter(item => item !== index);
+    result.set(index, [...new Set(array)]);
+  });
+  return result;
+};
+
+// context - содержимое файла
+// Определяет количество компонент связности преобразованного графа
+// выводит на экран состав каждой компоненты в форме списка входящих в компоненту вершин
+// Возвращяет массивы вершин
+const findComponentOfTheTransformedGraph = (context) => {
+  const result = [];
+  const simpleForm = graphToSimpleForm(context);
+  graphToSimpleForm(context).forEach((array, index) => {
+    if (_.flatten(result).indexOf(index) === -1) {
+      let preResult = _.uniq([index, ...array]);
+      preResult.forEach(i => preResult.push(simpleForm.get(i)));
+      preResult = _.uniq(_.flatten(preResult));
+      let isNew = true;
+      result.forEach((s, i1) => {
+        s.forEach((t) => {
+          if (preResult.indexOf(t) !== -1) {
+            result[i1] = _.uniq([...preResult, ...s]);
+            isNew = false;
+          }
+        });
+      });
+      if (isNew) {
+        result.push(preResult);
+      }
+    }
+  });
+  return result;
+};
+
+export {
+  createAdjacencyList,
+  stringToAdjacencyMatrix,
+  getDegreesOfVertices,
+  getIsolatedVertices,
+  getPendantVertices,
+  getLoops,
+  getMultipleVerges,
+  graphToSimpleForm,
+  findComponentOfTheTransformedGraph,
+};
